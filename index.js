e318cf1482d0f3fb0dc96159239011200365652c
Diane Leigh <leighd2008@gmail.com>
2023-01-16 11:50:11 -0500

    electron todo app

---
+++ index.js
@@ -0,0 +1,38 @@
+const electron = require('electron');
+
+const { app, BrowserWindow, Menu } = electron;
+
+let mainWindow
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    }
+  });
+  mainWindow.loadURL(`file://${__dirname}/main.html`);
+  
+  const mainMenu = Menu.buildFromTemplate(menuTemplate)
+  Menu.setApplicationMenu(mainMenu);
+});
+
+const menuTemplate = [
+  {
+    label: 'File',
+    submenu: [
+      { label: 'New Todo' },
+      {
+        label: 'Quit',
+        accelerator: process.platform === 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+        click() {
+          app.quit();
+        }
+      }
+    ]
+  }
+];
+
+if (process.platform === 'darwin') {
+  menuTemplate.unshift({});
+}
